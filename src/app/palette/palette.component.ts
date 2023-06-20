dd18a9db756e66e4e9d6d8b1f543c099a449b2c1
KilliannH <killiannhervagault@gmail.com>
2023-06-20 15:06:08 +0200

    working on palette

--- src/app/palette/palette.component.ts
+++ src/app/palette/palette.component.ts
@@ -8,10 +8,29 @@ import { Color } from 'src/types/color';
 })
 export class PaletteComponent {
   colors: Color[] = [
-    {id: 1, name: "Yellow", code: "#FFEB3B", selected: true},
-    {id: 1, name: "Red", code: "#F44336", selected: false},
-    {id: 2, name: "Blue", code: "#3F51B5", selected: false},
-    {id: 3, name: "Green", code: "#4CAF50", selected: false}
+    {name: "Yellow", code: "#FFEB3B", selected: true},
+    {name: "Red", code: "#F44336", selected: false},
+    {name: "Blue", code: "#3F51B5", selected: false},
+    {name: "Green", code: "#4CAF50", selected: false}
   ]
 
+  selectedColorIndex: number = 0;
+
+  _onClick(elm: any) {
+    this.selectedColorIndex = this.colors.findIndex((x) => {
+      return x.name === elm.id;
+    });
+    
+    for(let i = 0; i < this.colors.length; i++) {
+      if(i === this.selectedColorIndex) {
+        this.colors[i].selected = true;
+      } else {
+        this.colors[i].selected = false;
+      }
+    }
+  }
+
+  public getSelectedColor() {
+    return this.colors[this.selectedColorIndex];
+  }
 }
